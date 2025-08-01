7c33921cd4b347823682dc65b44c5387df9080bc
reyhaonan <reyhaonan2003@gmail.com>
2025-08-01 22:15:43 +0700

    init: trade

---
+++ src/Enums/TradeStatus.cs
@@ -0,0 +1,10 @@
+namespace MonopolyServer.Enums;
+
+public enum TradeStatus
+{
+    WaitForApproval,
+    Rejected,
+    Accepted,
+    InProcess,
+    Completed
+}
--- src/Models/Trade.cs
+++ src/Models/Trade.cs
@@ -1,1 +1,41 @@
+using System.Text.Json.Serialization;
+using MonopolyServer.Enums;
+
 namespace MonopolyServer.Models;
+
+public class Trade
+{
+    [JsonInclude]
+    public Guid TradeGuid { get; init; }
+    [JsonInclude]
+    public TradeStatus Status { get; private set; }
+    [JsonInclude]
+    public Guid InitiatorGuid{ get; init; }
+    [JsonInclude]
+    public Guid RecipientGuid{ get; init; }
+
+    [JsonInclude]
+    public List<Guid> PropertyOffer{ get; private set; }
+    [JsonInclude]
+    public List<Guid> PropertyCounterOffer{ get; private set; }
+
+    [JsonInclude]
+    public decimal MoneyFromInitiator{ get; private set; }
+    [JsonInclude]
+    public decimal MoneyFromRecipient{ get; private set; }
+
+    public Trade(Guid initiatorGuid, Guid recipientGuid, List<Guid> propertyOffer, List<Guid> propertyCounterOffer)
+    {
+        TradeGuid = Guid.NewGuid();
+
+        InitiatorGuid = initiatorGuid;
+
+        RecipientGuid = recipientGuid;
+
+        PropertyOffer = propertyOffer;
+
+        PropertyCounterOffer = propertyCounterOffer;
+
+        Status = TradeStatus.WaitForApproval;
+    }
+}
