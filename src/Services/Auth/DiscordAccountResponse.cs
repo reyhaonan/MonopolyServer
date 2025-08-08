b8a95ba036425091e6f19642572e8f45672a82af
reyhaonan <reyhaonan2003@gmail.com>
2025-08-08 19:17:58 +0700

    fix: response

--- src/Services/Auth/DiscordAccountResponse.cs
+++ src/Services/Auth/DiscordAccountResponse.cs
@@ -3,12 +3,12 @@ using System.Text.Json.Serialization;
 
 namespace MonopolyServer.Services.Auth;
 
-public record DiscordAccountResponse(string id, string username, string avatar)
+public record DiscordAccountResponse(string id, string global_name, string avatar)
 {
     [JsonInclude]
     public string id = id;
     [JsonInclude]
-    public string username = username;
+    public string global_name = global_name;
     [JsonInclude]
     public string avatar = avatar;
 }
